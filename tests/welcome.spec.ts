46db755415823248d441740227288ef8f9c35d7e
SySagar <sysagar07@gmail.com>
2024-02-23 01:40:16 +0530

    feat: chai and mocha

--- tests/welcome.spec.ts
+++ tests/welcome.spec.ts
@@ -1,11 +1,12 @@
 import { expect, use,request } from 'chai';
+import {describe, it} from 'mocha';
 import chaiHttp from 'chai-http';
 import server from '../index';
 
 use(chaiHttp);
 
 describe('welcome routes', function () {
-    it('welcome routes functional', (done) => {
+    it('welcome routes functional', (done: Mocha.Done) => {
         request(server)
             .get('/welcome')
             .end((err, res) => {
